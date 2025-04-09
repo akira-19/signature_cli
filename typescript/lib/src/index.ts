ebda3db0b4c0e40b9e496e3bf0f65698323326fb
Akira T <akira.tsuruoka.19@gmail.com>
2025-04-10 08:42:49 +0900

    add tx reader func

--- typescript/lib/src/index.ts
+++ typescript/lib/src/index.ts
@@ -1,13 +1,9 @@
 #!/usr/bin/env ts-node
 
 import { ethers } from 'ethers';
-import { readAndValidateTransaction } from './tx-reader';
-import * as fs from 'fs';
+import { readAndValidateTransaction, readTxJsonFromFile } from './tx-reader';
 
 async function main() {
-  // cli引数を取得
-  const path = process.argv[2];
-
   const privKey = process.env.PRIVATE_KEY;
 
   if (!privKey) {
@@ -16,8 +12,8 @@ async function main() {
 
   const wallet = new ethers.Wallet(privKey);
 
-  const rawData = fs.readFileSync(path, 'utf-8');
-  const jsonData = JSON.parse(rawData);
+  const file = process.argv[2];
+  const jsonData = readTxJsonFromFile(file);
   const tx = readAndValidateTransaction(jsonData);
 
   const signedTx = await wallet.signTransaction(tx);
