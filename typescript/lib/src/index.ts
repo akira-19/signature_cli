149d7d1b98449141f06cd30ea2b2599c2e7f0eed
Akira T <akira.tsuruoka.19@gmail.com>
2025-04-09 16:16:58 +0900

    remove wallet

--- typescript/lib/src/index.ts
+++ typescript/lib/src/index.ts
@@ -5,21 +5,18 @@ import { readAndValidateTransaction } from './tx-reader';
 import * as fs from 'fs';
 
 async function main() {
+  // cli引数を取得
+  const path = process.argv[2];
+
   const privKey = process.env.PRIVATE_KEY;
-  const rpcUrl = process.env.RPC_URL;
 
   if (!privKey) {
     throw new Error('PRIVATE_KEY is not set');
   }
 
-  if (!rpcUrl) {
-    throw new Error('RPC_URL is not set');
-  }
+  const wallet = new ethers.Wallet(privKey);
 
-  const provider = new ethers.JsonRpcProvider(rpcUrl);
-  const wallet = new ethers.Wallet(privKey, provider);
-
-  const rawData = fs.readFileSync('./tx.json', 'utf-8');
+  const rawData = fs.readFileSync(path, 'utf-8');
   const jsonData = JSON.parse(rawData);
   const tx = readAndValidateTransaction(jsonData);
 
